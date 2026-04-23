63c4e79e6cdc5930018ef591b2175e3aabe04ac3
UpstandingVlad <164867745+UpstandingVlad@users.noreply.github.com>
2026-04-23 16:23:26 +0800

    Added reading and parsing of rules txt file

--- fileparser.py
+++ fileparser.py
@@ -1,5 +1,6 @@
 import os 
 import argparse 
+import pathlib
 
 parser = argparse.ArgumentParser()
 parser.add_argument("Input", help="The input csv file to parse")
@@ -9,3 +10,22 @@ parser.add_argument("-v", "--verbose", action="count", help="Makes output more v
 args = parser.parse_args()
 
 print(args)
+InputFile = args.Input
+Rules = []
+ParsedRules = [] 
+
+try:
+	with open(args.Rules, 'r') as f:
+		Rules = f.readlines()
+except FileNotFoundError as e:
+	print(f"Error opening rules file: {e}")
+except Exception as e:
+	print(f"Error: {e}")
+
+if Rules:
+	for line in Rules:
+		Rule, Args = line.rstrip().split(":")
+		Args = Args.split(",")
+		ParsedRules.append([Rule,Args])
+
+print(ParsedRules)
