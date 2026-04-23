b799551af9021250409cdb96c327da4e1bf930c8
UpstandingVlad <164867745+UpstandingVlad@users.noreply.github.com>
2026-04-23 17:02:15 +0800

    Added dynamic import of py files from rules subdirectory, allowing for scalable and modular rules to be implemented with easy scalability

--- fileparser.py
+++ fileparser.py
@@ -1,12 +1,25 @@
 import os 
-import argparse 
+import sys
+import argparse
 import pathlib
+import importlib.util
+
+
+def import_from_dir(directory):
+    for py_file in pathlib.Path(directory).glob("*.py"):
+        module_name = py_file.stem 
+        spec = importlib.util.spec_from_file_location(module_name, py_file)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        sys.modules[module_name] = module 
+        globals()[module_name] = module
+
+import_from_dir("Rules")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("Input", help="The input csv file to parse")
 parser.add_argument("Rules", help="The rules for which to parse the input csv file")
 parser.add_argument("-v", "--verbose", action="count", help="Makes output more verbose by generating an additional output file detailing which rules each row of the failed output csv failed")
-
 args = parser.parse_args()
 
 print(args)
